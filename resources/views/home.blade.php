e1a4f55766d0a60eb28343f752db42809f4bbb47
chawki <karbachi.chawki@ayrade.com>
2024-01-08 21:43:25 +0100

    add Iframe link

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,7 +16,15 @@
                     <div class="col-xl-12">
                         <div class="card">
                             <div class="card-body row" style="margin:0px;padding:0px;overflow:hidden "  height="100%" width="100%" >
-                                <iframe src="https://youtube.com" frameborder="0" style="overflow:hidden;height:700px;width:100%" height="100%" width="100%"> </iframe>
+
+                                <iframe src="https://public.zenkit.com/f/e1Toduedq/form-havaleh?v=SKs0oca77"
+                                        style="width: 100%; min-height: 950px;background: transparent; "
+                                        allowfullscreen>
+                                        <a href="https://public.zenkit.com/f/e1Toduedq/form-havaleh?v=SKs0oca77" target="_blank">
+                                            Your browser doesn't support iframes. Click here to open the content in a new tab.
+                                      </a>
+                                    </iframe>
+
                             </div>
                         </div>
                     </div>
